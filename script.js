912b970c25f1c82da24f5152f7713ca45c261542
@amritmohan <amritmohan2284@gmail.com>
2025-02-08 21:11:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
+    
+    const obj = {
+        0: ['M', 1000],
+        1: ['CM', 900],
+        2: ['D', 500],
+        3: ['CD', 400],
+        4: ['C', 100],
+        5: ['XC', 90],
+        6: ['L', 50],
+        7: ['XL', 40],
+        8: ['X', 10],
+        9: ['IX', 9],
+        10: ['V', 5],
+        11: ['IV', 4],
+        12: ['I', 1]
     };
-
-  //your code here
+    
+    if (num < 0 || num > 100000) return "Out of range";
+    
 	let result = "";
-  let i = 0;
-  while (num > 0) {
-    if (num >= obj[i][1]) {
-      result += obj[i][0];
-      num -= obj[i][1];
-    } else if (i % 2 == 0 && num >= obj[i][1] - obj[i + 2][1]) {
-      result += obj[i + 2][0] + obj[i][0];
-      num -= obj[i][1] - obj[i + 2][1];
-    } else if (i % 2 == 1 && num >= obj[i][1] - obj[i + 1][1]) {
-      result += obj[i + 1][0] + obj[i][0];
-      num -= obj[i][1] - obj[i + 1][1];
-    } else {
-      i++;
+    
+    for (let key in obj) {
+        let [symbol, value] = obj[key];
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
     }
-    i++;
-  }
-  return result;
-
+    
+    return result;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 

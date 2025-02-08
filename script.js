25cd4faa91288d0d7f4a7f5fb47c9088f99020cb
@amritmohan <amritmohan2284@gmail.com>
2025-02-08 21:07:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,24 @@ function convertToRoman(num) {
     };
 
   //your code here
+	let result = "";
+  let i = 0;
+  while (num > 0) {
+    if (num >= obj[i][1]) {
+      result += obj[i][0];
+      num -= obj[i][1];
+    } else if (i % 2 == 0 && num >= obj[i][1] - obj[i + 2][1]) {
+      result += obj[i + 2][0] + obj[i][0];
+      num -= obj[i][1] - obj[i + 2][1];
+    } else if (i % 2 == 1 && num >= obj[i][1] - obj[i + 1][1]) {
+      result += obj[i + 1][0] + obj[i][0];
+      num -= obj[i][1] - obj[i + 1][1];
+    } else {
+      i++;
+    }
+    i++;
+  }
+  return result;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
